c5d932a31e701b8aa4fde5352190d3d9a80659d5
nopoxxx <dmitriy.nopox@gmail.com>
2023-12-26 19:42:08 +1100

    add generate and clear buttons

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -1,10 +1,15 @@
-
-window.onload = function()
-{
+document.getElementById('generate').addEventListener('click', function () {
     const initPerson = personGenerator.getPerson();
     document.getElementById('jobOutput').innerText = initPerson.job;
     document.getElementById('FullnameOutput').innerText = initPerson.name;
     document.getElementById('genderOutput').innerText = initPerson.gender;
     document.getElementById('birthOutput').innerText = initPerson.birth;
-};
+})
 
+document.getElementById('clear').addEventListener('click', function () {
+    const initPerson = personGenerator.getPerson();
+    document.getElementById('jobOutput').innerText = "Генерация профессии";
+    document.getElementById('FullnameOutput').innerText = "Генерация ФИО";
+    document.getElementById('genderOutput').innerText = "Генерация пола";
+    document.getElementById('birthOutput').innerText = "Генерация даты рождения";
+})
